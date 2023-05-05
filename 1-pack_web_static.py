7cf78e31df99a4d1ce544f9e35016f1f9432195a
Barissa-Imran <imranbarissa@gmail.com>
2023-05-05 15:44:06 +0300

    add task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,6 +6,7 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """Create a .tgz archive of the directory web_static"""
     date = datetime.utcnow()
